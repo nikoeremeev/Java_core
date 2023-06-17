6babdad894c101c78354f638d4bf4270b1cf9629
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-06-17 11:02:40 +0300

    add tasks for lesson 5

---
+++ src/main/java/ru/geekbrains/lesson5/Tree.java
@@ -0,0 +1,46 @@
+package ru.geekbrains.lesson5;
+
+import java.io.File;
+
+public class Tree {
+
+    /**
+     * TODO: Доработать метод print, необходимо распечатывать директории и файлы
+     * @param file
+     * @param indent
+     * @param isLast
+     */
+    public static void print(File file, String indent, boolean isLast){
+        System.out.print(indent); // рисуем отступ
+        if (isLast){
+            System.out.print("└─");
+            indent += "  ";
+        }
+        else {
+            System.out.print("├─");
+            indent += "│ ";
+        }
+        System.out.println(file.getName());
+
+        File[] files = file.listFiles();
+        if (files == null)
+            return;
+
+        int subDirTotal = 0;
+        for (int i = 0; i < files.length; i++){
+           if (files[i].isDirectory())
+               subDirTotal++;
+        }
+
+        int subDirCounter = 0;
+        for (int i = 0; i < files.length; i++){
+            if (files[i].isDirectory()){
+                print(files[i], indent, subDirCounter  == subDirTotal - 1);
+                subDirCounter++;
+            }
+        }
+
+
+    }
+
+}
